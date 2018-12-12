76c4a823869621b2c64715206b206fd39724e761
Pierre <info@plouton.co.za>
2018-12-12 19:08:34 +0200

    Initial Commit

---
+++ FileIO/FileIO.h
@@ -0,0 +1,19 @@
+//
+//  FileIO.h
+//  FileIO
+//
+//  Created by Pierre Oosthuizen on 2018/12/10.
+//  Copyright © 2018 Pieter C. Oosthuizen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FileIO.
+FOUNDATION_EXPORT double FileIOVersionNumber;
+
+//! Project version string for FileIO.
+FOUNDATION_EXPORT const unsigned char FileIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileIO/PublicHeader.h>
+
+
---
+++ FileIO/FileIO.swift
@@ -0,0 +1,71 @@
+//
+//  FileIO.swift
+//
+//  Created by Pierre Oosthuizen on 2018/08/16.
+//  Copyright © 2018 Plouton Consulting. All rights reserved.
+//
+//
+//  MainWC.swift
+//  FinCalc
+//
+//  Created by Pierre Oosthuizen on 2018/08/10.
+//  Copyright © 2018 Pieter C. Oosthuizen. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Class allows reading from PList files into NSDictionary and NSArray.
+ */
+open class FileIO: NSObject {
+    
+    /**
+     Reads a plist file and returns the results in an array.
+     
+     - Parameter plistFile: The file to be read for array values.
+     
+     - Returns: A new string array containing all the values from the file.
+     */
+    public func readDataArrayFromFile(plistFile: String)-> [String] {
+        
+        var arrayRecords: [String] = []
+        
+        if let fileURL = Bundle.main.url(forResource: plistFile, withExtension: "plist") {
+            arrayRecords = NSArray(contentsOf: fileURL)! as! [String]
+        }
+        return (arrayRecords)
+    }
+    
+    /**
+     Reads a plist file and returns the results in a dictionary.
+     
+     - Parameter plistFile: The file to be read for dictionary values.
+     
+     - Returns: A new dictionary containing all the values from the file.
+     */
+    public func readDataDictionaryFromFile(plistFile: String) -> NSDictionary {
+        
+        var dictRecords: NSDictionary = [:]
+        
+        if let fileURL = Bundle.main.url(forResource: plistFile, withExtension: "plist") {
+            dictRecords = NSDictionary(contentsOf: fileURL)! as NSDictionary
+        }
+        return (dictRecords)
+        
+    }
+    
+    /**
+     Reads a plist file and returns the results in either dictionary or array.
+     
+     - Parameter dataFile: The file to be read for dictionary values.
+     
+     - Returns: A new array or dictionary containing all the values from the file.
+     */
+    public func readDataFromPList<T>(dataFile: String) -> T {
+        
+        let fileURL = Bundle.main.url(forResource: dataFile, withExtension: "plist")
+        let fileContent: T = NSDictionary(contentsOf: fileURL!) as! T
+        
+        return (fileContent)
+    }
+}
